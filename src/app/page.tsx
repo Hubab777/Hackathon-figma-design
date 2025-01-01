01247ab62e3aeb2b2ca4818cf6b689e7cff0f745
Hubab777 <159882654+Hubab777@users.noreply.github.com>
2025-01-01 20:18:32 +0500

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,39 +27,3 @@ const page = () => {
 
 export default page
 
-// import React from 'react';
-// import Features from './components/features';
-// import Latestproduct from './components/latestproduct';
-// import TopBar from './components/navbar';
-// import Services from './components/services';
-// import HeroSection from './components/hero';
-// import Header from './components/header';
-// import Trendingproduct from './components/trendingproduct';
-// import Trending from './components/trending';
-// import Discountitem from './components/discountitem';
-// import Category from './components/category';
-// import Blog from './components/blog';
-// 
-// 
-// 
-// function page() {
-  // return (
-    // <div>
-{/*    */}
-  {/* <TopBar/> */}
-     {/* <Header/>  */}
-     {/* <HeroSection/> */}
-     {/* <Features/> */}
-     {/* <Latestproduct/> */}
-     {/* <Services/> */}
-     {/* <Trendingproduct/> */}
-     {/* <Trending/> */}
-      
-     {/* <Discountitem/> */}
-     {/* <Category/> */}
-     {/* <Blog/> */}
-    {/* </div> */}
-  // )
-// }
-// 
-// export default page
