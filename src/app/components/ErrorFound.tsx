deeabc84f8dc5c6bb400e42de1e391d1cda141a9
Hubab777 <159882654+Hubab777@users.noreply.github.com>
2025-01-04 00:32:40 +0500

    Update ErrorFound.tsx

--- src/app/components/ErrorFound.tsx
+++ src/app/components/ErrorFound.tsx
@@ -3,9 +3,25 @@ import Image from "next/image";
 
 function ErrorPage() {
   return (
-    <div className="justify-center items-center md: place-items-center wrapper">
+    <div className=" bg-gray-50 min-h-screen wrapper">
+      {/* Header */}
+
+      <header className="bg-purple-100 text-center py-8">
+        <div className="container mx-auto px-4">
+          {/* Title */}
+          <h1 className="text-3xl font-bold text-left pl-20 text-[#111C85]">Error Page</h1>
+          {/* Breadcrumb */}
+          <p className="text-sm text-gray-500 text-left pl-20 mt-2">
+            <span className="text-[#727272]">Home .</span>
+            
+            <span className="text-[#727272]">Pages</span>
+            
+            <span className="text-pink-500 font-semibold"> .ErrorFound</span>
+          </p>
+        </div>
+      </header>
       
-      <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center md: place-items-center min-h-screen">
         <Image
           src="/err.png"
           alt="404"
